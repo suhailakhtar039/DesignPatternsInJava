0e115fc76e6198f593850e571e2f58c5d2097b3c
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-11-03 23:01:45 +0530

    basic functionality of equals and hashcode

--- com/example/helloworld/practice/TwoSum.java
+++ com/example/helloworld/practice/TwoSum.java
@@ -1,8 +1,32 @@
 package com.example.helloworld.practice;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
+class Abc {
+    private int a;
+    public int getA() {
+        return a;
+    }
 
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Abc abc = (Abc) o;
+        return a == abc.a;
+    }
+
+    @Override
+    public int hashCode() {
+        return a;
+    }
+}
 public class TwoSum {
     public static int[] twosum(int[] nums,int target){
         int n = nums.length;
@@ -20,15 +44,17 @@ public class TwoSum {
     }
     public static void main(String[] args) {
         int n,target;
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        target = sc.nextInt();
-        int []a = new int[n];
-        for(int i=0; i<n; i++)
-            a[i] = sc.nextInt();
-        int []ans;
-        ans = twosum(a,target);
-        System.out.println(ans[0]);
-        System.out.println(ans[1]);
+        Abc a = new Abc();
+        a.setA(3);
+        Abc b = new Abc();
+        b.setA(4);
+        System.out.println(a.getClass());
+        Map<Abc,Integer> m = new HashMap<>();
+        m.put(a,1);
+        m.put(b,1);
+        System.out.println(m.size());
+        for(Map.Entry<Abc,Integer> q:m.entrySet()){
+            System.out.println(q.getKey()+"-"+q.getValue());
+        }
     }
 }
