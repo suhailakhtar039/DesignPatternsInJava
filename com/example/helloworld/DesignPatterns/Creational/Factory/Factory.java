301c3c145d939ff909a1ae5ce1a1fb6cf8fd16a2
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-11-17 23:32:05 +0530

    nested class in factory

--- com/example/helloworld/DesignPatterns/Creational/Factory/Factory.java
+++ com/example/helloworld/DesignPatterns/Creational/Factory/Factory.java
@@ -8,14 +8,6 @@ class Point {
         this.y = y;
     }
 
-    public static Point newCartesianPoint(double x,double y){
-        return new Point(x,y);
-    }
-
-    public static Point newPolarPoint(double rho,double theta){
-        return new Point(rho*Math.cos(theta),rho*Math.sin(theta));
-    }
-
     @Override
     public String toString() {
         return "Point{" +
@@ -23,12 +15,22 @@ class Point {
                 ", y=" + y +
                 '}';
     }
-}
+    public static class Factory {
+        public static Point newCartesianPoint(double x,double y){
+            return new Point(x,y);
+        }
 
+        public static Point newPolarPoint(double rho,double theta){
+            return new Point(rho*Math.cos(theta),rho*Math.sin(theta));
+        }
+    }
+}
 public class Factory {
     public static void main(String[] args) {
-        Point point = Point.newPolarPoint(4, 2);
-        point = Point.newCartesianPoint(4,5);
-        System.out.println(point);
+        // Point point = Point.newPolarPoint(4, 2);
+        // point = Point.newCartesianPoint(4,5);
+        // System.out.println(point);
+        Point point = Point.Factory.newCartesianPoint(3, 4);
+        Point point1 = Point.Factory.newPolarPoint(2, 0.4);
     }
 }
