7f802e56c2589196fa4a5f23f6e7fb1af21680b8
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-11-25 00:06:10 +0530

    structural demo is completed with caching

--- com/example/helloworld/DesignPatterns/Structural/StructuralDemo.java
+++ com/example/helloworld/DesignPatterns/Structural/StructuralDemo.java
@@ -1,11 +1,10 @@
 package com.example.helloworld.DesignPatterns.Structural;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 
-class Point{
-    public int x,y;
+class Point {
+    public int x, y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -19,39 +18,75 @@ class Point{
                 ", y=" + y +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
 
-class Line{
-    public Point start,end;
+class Line {
+    public Point start, end;
 
     public Line(Point start, Point end) {
         this.start = start;
         this.end = end;
     }
-}
 
-class VectorObject extends ArrayList<Line>{
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-}
+        Line line = (Line) o;
 
-class VectorRectangle extends VectorObject{
-    public VectorRectangle(int x,int y,int width,int height) {
-        add(new Line(new Point(x,y),new Point(x+width,y)));
-        add(new Line(new Point(x+width,y),new Point(x+width,y+height)));
-        add(new Line(new Point(x,y),new Point(x,y+height)));
-        add(new Line(new Point(x,y+height),new Point(x+width,y+height)));
+        if (!start.equals(line.start)) return false;
+        return end.equals(line.end);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start.hashCode();
+        result = 31 * result + end.hashCode();
+        return result;
     }
 }
 
-class LineToPointAdapter extends ArrayList<Point>{
+class VectorObject extends ArrayList<Line> {
+
+}
+
+class VectorRectangle extends VectorObject {
+    public VectorRectangle(int x, int y, int width, int height) {
+        add(new Line(new Point(x, y), new Point(x + width, y)));
+        add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+        add(new Line(new Point(x, y), new Point(x, y + height)));
+        add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+    }
+}
+
+class LineToPointAdapter implements Iterable<Point> {
     private static int count = 0;
+    private static Map<Integer, List<Point>> cache = new HashMap<>();
+    private int hash;
+    public LineToPointAdapter(Line line) {
+        hash = line.hashCode();
+        if(cache.containsKey(hash)) return;
 
-    public LineToPointAdapter(Line line)
-    {
         System.out.println(
-                String.format("%d: Generating points for line [%d,%d]-[%d,%d] (no caching)",
+                String.format("%d: Generating points for line [%d,%d]-[%d,%d] (with caching)",
                         ++count, line.start.x, line.start.y, line.end.x, line.end.y));
-
+        ArrayList<Point> points = new ArrayList<>();
         int left = Math.min(line.start.x, line.end.x);
         int right = Math.max(line.start.x, line.end.x);
         int top = Math.min(line.start.y, line.end.y);
@@ -59,47 +94,58 @@ class LineToPointAdapter extends ArrayList<Point>{
         int dx = right - left;
         int dy = line.end.y - line.start.y;
 
-        if (dx == 0)
-        {
-            for (int y = top; y <= bottom; ++y)
-            {
-                add(new Point(left, y));
+        if (dx == 0) {
+            for (int y = top; y <= bottom; ++y) {
+                points.add(new Point(left, y));
+            }
+        } else if (dy == 0) {
+            for (int x = left; x <= right; ++x) {
+                points.add(new Point(x, top));
             }
         }
-        else if (dy == 0)
-        {
-            for (int x = left; x <= right; ++x)
-            {
-                add(new Point(x, top));
-            }
-        }
+        cache.put(hash,points);
     }
 
+    @Override
+    public Iterator<Point> iterator() {
+        return cache.get(hash).iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Point> action) {
+        cache.get(hash).forEach(action);
+    }
+
+    @Override
+    public Spliterator<Point> spliterator() {
+        return cache.get(hash).spliterator();
+    }
 }
 
 public class StructuralDemo {
 
     private final static List<VectorObject> vectorObjects = new ArrayList<>(
             Arrays.asList(
-                    new VectorRectangle(1,1,10,10),
-                    new VectorRectangle(3,3,6,6)
+                    new VectorRectangle(1, 1, 10, 10),
+                    new VectorRectangle(3, 3, 6, 6)
             )
     );
 
-    private static void draw(){
-        for(VectorObject vo : vectorObjects){
-            for(Line line:vo){
+    private static void draw() {
+        for (VectorObject vo : vectorObjects) {
+            for (Line line : vo) {
                 LineToPointAdapter adapter = new LineToPointAdapter(line);
                 adapter.forEach(StructuralDemo::drawPoint);
             }
         }
     }
 
-    public static void drawPoint(Point p){
+    public static void drawPoint(Point p) {
         System.out.println(".");
     }
 
     public static void main(String[] args) {
+        draw();
         draw();
     }
 }
