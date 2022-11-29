02d0e7179674a31066c09595287e3ac7af09c08a
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-11-29 23:39:14 +0530

    Static string decorator created

--- com/example/helloworld/DesignPatterns/Structural/Decorator/StringDecoratorStatic.java
+++ com/example/helloworld/DesignPatterns/Structural/Decorator/StringDecoratorStatic.java
@@ -1,10 +1,12 @@
 package com.example.helloworld.DesignPatterns.Structural.Decorator;
 
+import java.util.function.Supplier;
+
 interface Shape {
     String info();
 }
 
-class Circle implements ShapeDynamic {
+class Circle implements Shape {
 
     private float radius;
 
@@ -25,7 +27,7 @@ class Circle implements ShapeDynamic {
     }
 }
 
-class Square implements ShapeDynamic {
+class Square implements Shape {
     private float side;
 
     public Square() {
@@ -42,32 +44,30 @@ class Square implements ShapeDynamic {
 }
 
 // making a decorator
-class ColoredShape implements ShapeDynamic {
+class ColoredShape<T extends Shape> implements Shape {
 
-    private ShapeDynamic shape;
+    private Shape shape;
     private String color;
 
-    public ColoredShape(ShapeDynamic shape, String color) {
-        this.shape = shape;
+    public ColoredShape(Supplier<? extends T> ctor,String color) {
+        shape = ctor.get();
         this.color = color;
     }
 
     @Override
     public String info() {
-        return shape.info() + " has the color of " + color;
+        return shape.info() + " has a color " + color;
     }
 }
 
-class TransparentShape implements ShapeDynamic {
+class TransparentShape<T extends Shape> implements Shape{
 
-    private ShapeDynamic shape;
+    private Shape shape;
     private int transparency;
-
-    public TransparentShape(ShapeDynamic shape, int transparency) {
-        this.shape = shape;
+    public TransparentShape(Supplier<? extends T> ctor,int transparency){
+        shape = ctor.get();
         this.transparency = transparency;
     }
-
     @Override
     public String info() {
         return shape.info() + " has " + transparency + "% transparency";
@@ -76,6 +76,14 @@ class TransparentShape implements ShapeDynamic {
 
 public class StringDecoratorStatic {
     public static void main(String[] args) {
+        ColoredShape<Square> blueSquare = new ColoredShape<>(
+                () -> new Square(10),
+                "blue"
+        );
+        System.out.println(blueSquare.info());
 
+        TransparentShape<ColoredShape<Circle>> myCircle = new TransparentShape<>(()->
+                new ColoredShape<>(()->new Circle(20),"green"),50);
+        System.out.println(myCircle.info());
     }
 }
