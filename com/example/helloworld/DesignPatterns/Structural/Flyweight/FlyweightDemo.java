3b808915db9830f8a7fdc3e0fb784162df92148e
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-12-06 23:17:55 +0530

    saving memory using flyweight

--- com/example/helloworld/DesignPatterns/Structural/Flyweight/FlyweightDemo.java
+++ com/example/helloworld/DesignPatterns/Structural/Flyweight/FlyweightDemo.java
@@ -1,4 +1,38 @@
 package com.example.helloworld.DesignPatterns.Structural.Flyweight;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+class User{
+    private String fullName;
+
+    public User(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+class User2{
+    static List<String> strings = new ArrayList<>();
+    private int[] names;
+    public User2(String fullName){
+        Function<String, Integer> getOrAdd = (String s)->{
+            int idx = strings.indexOf(s);
+            if(idx != -1)return idx;
+            strings.add(s);
+            return strings.size()-1;
+        };
+
+        names = Arrays.stream(fullName.split(" "))
+                .mapToInt(s -> getOrAdd.apply(s))
+                .toArray();
+
+    }
+}
+
 public class FlyweightDemo {
+    public static void main(String[] args) {
+        User2 u1 = new User2("Suhail Akhtar");
+    }
 }
